fbb9f3ea37169bc18845a116504cdb813b47ce4c
frlegg <84002781+frlegg@users.noreply.github.com>
2022-09-05 14:17:56 +1000

    Haha, typo fix.

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function getNextScenario(scenarioNumber) {
     $.getJSON("./scenarios/" + scenarioNumber + ".json").done(function(data) {
         $("body").append("\n" + data["text"])
         for (var i = 0; i < data["connections"].length; i++) {
-            $("body").append(`<button onelick="getNextScenario(` + data["connections"][i].toString() + `)">` + data["connections"][i].toString() + `</button>`)
+            $("body").append(`<button onclick="getNextScenario(` + data["connections"][i].toString() + `)">` + data["connections"][i].toString() + `</button>`)
         }
     });
 }
