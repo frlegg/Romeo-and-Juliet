0163482ebc6c87b52df2f356c9fa53f725dc307b
frlegg <84002781+frlegg@users.noreply.github.com>
2022-09-05 14:44:03 +1000

    Clear text when scenario loaded

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 })
 
 function getNextScenario(scenarioNumber) {
-    $("button").remove()
+    $("body").html("")
     $.getJSON("./scenarios/" + scenarioNumber + ".json").done(function(data) {
         $("body").append(data["text"] + "<br>")
         for (var i = 0; i < data["connections"].length; i++) {
