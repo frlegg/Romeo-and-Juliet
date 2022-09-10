8c6e1405abfee9af58fa606f3e760e36e9b3222b
frlegg <84002781+frlegg@users.noreply.github.com>
2022-09-10 11:31:10 +1000

    Me dum-dum. Fixed bug-bug

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ $(document).ready(function() {
 function getNextScenario(scenarioNumber) {
     $("body").html("")
     $.getJSON("./scenarios/" + scenarioNumber + ".json").done(function(data) {
+        $("body").append("<h1>Chapter " + scenarioNumber + "</h1><br>")
         $("body").append(data["text"] + "<br>")
         for (var i = 0; i < data["connections"].length; i++) {
             try {
-                $("body").prepend("<h1>Chapter " + data["connections"][i]["number"] + "</h1><br><br>")
                 $("#buttons").append(`<button onclick="getNextScenario(` + data["connections"][i]["number"].toString() + `)">` + data["connections"][i]["prompt"].toString() + `</button>`)
             } catch (error) {
-                $("body").prepend("<h1>Chapter " + data["connections"][i] + "</h1><br><br>")
                 $("#buttons").append(`<button onclick="getNextScenario(` + data["connections"][i].toString() + `)">` + data["connections"][i].toString() + `</button>`)
             }
         }
