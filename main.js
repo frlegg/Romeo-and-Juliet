7e9f421e11ba419b95830e2bb2b1614853d93d89
frlegg <84002781+frlegg@users.noreply.github.com>
2022-09-10 11:50:24 +1000

    Fixed bug

--- main.js
+++ main.js
@@ -3,18 +3,11 @@ $(document).ready(function() {
 })
 
 function getNextScenario(scenarioNumber) {
-    $("body").html("")
+    $("body").html(`<div id="buttons"></div>`)
     $.getJSON("./scenarios/" + scenarioNumber + ".json").done(function(data) {
-        console.log(data)
         $("body").append(data["text"] + "<br>")
-        console.log(data["text"])
-        console.log(data["connections"])
-        console.log(data["connections"].length)
         for (var i = 0; i < data["connections"].length; i++) {
-            console.log(data["connections"][i])
             try {
-                console.log(data["connections"][i]["number"])
-                console.log(data["connections"][i]["prompt"])
                 $("#buttons").append(`<button onclick="getNextScenario(` + data["connections"][i]["number"].toString() + `)">` + data["connections"][i]["prompt"].toString() + `</button>`)
             } catch (error) {
                 $("#buttons").append(`<button onclick="getNextScenario(` + data["connections"][i].toString() + `)">` + data["connections"][i].toString() + `</button>`)
